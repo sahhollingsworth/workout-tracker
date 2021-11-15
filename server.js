c578c15cd45b27e74ad19e0eb8576373771eda56
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-11-14 16:42:28 -0800

    Set up express package parameters in server.js.
    Set up port 5660 and added listener

---
+++ server.js
@@ -0,0 +1,19 @@
+// Import required packages
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+// Default app PORT for use
+const PORT = process.env.PORT || 5660;
+
+const app = express();
+// Parse incoming requests with urlencoded payloads and return as objects
+app.use(express.urlencoded({ extended: true }));
+// Return the incoming request objects as JSON objects
+app.unsubscribe(express.json());
+// Serve static files in the public directory
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+    console.log('App listening on Port ${PORT}!');
+});
