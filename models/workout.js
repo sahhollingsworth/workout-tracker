151d6d1a8b751e2b7ecede37f06a0d2ea6242858
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-11-14 16:26:37 -0800

    Defined Workout model schema.
    Removed trailing whitespace from seeds.js

---
+++ models/workout.js
@@ -0,0 +1,50 @@
+// Import Mongoose package for schema development
+const mongoose = require("mongoose");
+
+// Use Mongoose Schema
+const Schema = mongoose.Schema;
+
+// Use schema to define workout document fields
+const workoutSchema = new Schema({
+    // Date is assigned the datetime value of document creation (user form entry)
+    day: {
+        type: Date,
+        default:Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                required: true,
+            },
+            name: {
+                type: String,
+                required: true,
+            },
+            duration:  {
+                type: Number,
+                required: true,
+            },
+            // only applies to resistance workouts
+            weight:  {
+                type: Number,
+            },
+            // only applies to resistance workouts
+            reps:  {
+                type: Number,
+            },
+            // only applies to resistance workouts
+            sets:  {
+                type: Number,
+            },
+            // only applies to cardio workouts
+            distance: {
+                type: Number,
+            }
+        }
+    ]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
