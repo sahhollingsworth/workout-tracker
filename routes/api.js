5394494d3ae303d89a3e92f8ec2a97847816a4eb
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-11-14 20:20:55 -0800

    Feat: Added route to create workout document

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,28 @@
 const router = require("express").Router();
 const Workout = require("../models/workout");
 
-// Get all workout documents
-// /api/workouts
-
 // Get exercise in a given workout by id
 
 // getWorkoutsInRange()
 // /api/workouts/range
 
+
 // Create new workout document - createWorkout(data = {})
 // /api/workouts
+router.post('/api/workouts', (req, res) => {
+    Workout.create({})
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
 
 // Add exercise object to most recent workout - addExercise(data)
 // /api/workouts/:id
+
 
 // Get last Workout - getLastWorkout()
 // /api/workouts
@@ -21,6 +30,4 @@ const Workout = require("../models/workout");
 
 
 
-
 module.exports = router;
-
