c8f357639a485498c96121eed82827d5150f67a7
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-11-15 16:00:29 -0800

    Install Dotenv package for seeding to mongodb database
    Added .env, added .env to .gitignore, updated seeds.js to use dot env for db access

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,8 +1,11 @@
 let mongoose = require("mongoose");
 let db = require("../models");
+require('dotenv').config();
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
+  // useUnifiedTopology: true,
+  // useCreateIndex: true,
   useFindAndModify: false
 });
 
